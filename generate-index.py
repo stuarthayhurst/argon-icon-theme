97ea48ee046ace6e2ef598f277711726fe727fbc
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-06-24 14:25:03 +0100

    Added missing module

--- generate-index.py
+++ generate-index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys, shutil, csv
+import os, sys, shutil, csv
 from icon_builder import createContextDict, orderDirs
 
 #Function to return array of all subdirectories of searchDir
