63da993fbee774962f783a03a236082784a692a0
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-08-12 11:44:48 +0100

    Allow removing attributes from chosen namespaces

--- clean-svgs.py
+++ clean-svgs.py
@@ -7,6 +7,7 @@ from icon_builder import getResolutionDirs
 
 buildDir = "argon"
 et = et.ElementTree()
+targetNamespaces = ["{http://www.inkscape.org/namespaces/inkscape}"]
 
 def cleanFile(inputFile):
   #Find metadata tag in document
@@ -16,12 +17,26 @@ def cleanFile(inputFile):
   #Remove if present
   if metadata != None:
     root.remove(metadata)
-    et.write(inputFile)
   else:
-    print(f"{file} has no metadata tag")
+    print(f"{inputFile} has no metadata tag")
+
+  #Find all attributes matching namespaces to remove
+  delKeys = []
+  for attribute in root.attrib:
+    for namespace in targetNamespaces:
+      if namespace in attribute:
+        delKeys.append(attribute)
+
+  #Remove the marked keys
+  for key in delKeys:
+    root.attrib.pop(key)
+
+  et.write(inputFile)
 
 #Loop through all svgs
 for file in glob.glob(f"{buildDir}/scalable/*/*"):
   cleanFile(file)
+
+#cleanFile("argon/scalable/mimetypes/application-vnd.sqlite3.svg")
 
 print("Cleaned all files")
