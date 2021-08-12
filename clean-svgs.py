c5f243b36fae4693a2062f475440a0af39fd3d25
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-08-12 10:52:42 +0100

    Renamed file and added comment

--- clean-svgs.py
+++ clean-svgs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#Remove rubbish from svg files
 
 import glob
 import xml.etree.ElementTree as et
@@ -8,16 +9,18 @@ buildDir = "argon"
 et = et.ElementTree()
 
 def cleanFile(inputFile):
+  #Find metadata tag in document
   root = et.parse(inputFile)
-
   metadata = root.find("{http://www.w3.org/2000/svg}metadata")
+
+  #Remove if present
   if metadata != None:
     root.remove(metadata)
     et.write(inputFile)
   else:
     print(f"{file} has no metadata tag")
 
-
+#Loop through all svgs
 for file in glob.glob(f"{buildDir}/scalable/*/*"):
   cleanFile(file)
 
